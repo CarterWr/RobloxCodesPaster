d214279cf45da46fdd030452f80ba9c3bd711610
Carter Wright <skycreed04@gmail.com>
2023-11-16 10:17:22 -0500

    Added manual option

--- RobloxCodesPaster.py
+++ RobloxCodesPaster.py
@@ -75,14 +75,52 @@ def auto_code_paster(lst):
     sleep(5)
     #Loop through each code in the list
     for code in lst:
-        pyautogui.write(code)
+        pyautogui.write(code) 
         pyautogui.press('enter')
         pyautogui.hotkey('ctrl', 'a')
         pyautogui.press('backspace')
+
+#Manual code pasting function
+def manual_code_paster(lst):
+    """
+    Takes in the extracted codes and pastes each code but with a more manual style
+
+    Params: lst(list): The codes_lst that we got from extracting codes from file
+
+    Returns: None
+    """
+    #Give user time to put cursor in the right spot 
+    sleep(5)
+    #Loop through each code in the list
+    for code in lst:
+        pyautogui.write(code)
+        #instead of pyautogui hitting the enter key we wait for user input
+        input("Press confirm or ok on the code box in game to claim the code")
+        #give the user time again to click back into the text box after claiming
+        sleep(3)
+        pyautogui.hotkey('ctrl', 'a')
+        pyautogui.press('backspace')
+
 
 #Execution code
 if __name__ == "__main__":
     #initalizing codes_lst to the list of codes returned by data_extractor()
     codes_lst = data_extractor()
-    #run the code_paster function to automate pasting codes
-    auto_code_paster(codes_lst)
+
+    #display options to user 
+    print("1 - auto paster (no clicking involved)")
+    print("2 - manual paster (you must click the confirm button to contiue the clicks)")
+
+    #gather user input after dispaly
+    user_input = int(input())
+
+    if user_input == 1:
+        #run the code_paster function to automate pasting codes
+        auto_code_paster(codes_lst)
+    elif user_input == 2:
+        #runs the manual version of the code paster
+        manual_code_paster(codes_lst)
+    else:
+        #if 1 or 2 is not selected run auto as default
+        auto_code_paster(codes_lst)
+    
