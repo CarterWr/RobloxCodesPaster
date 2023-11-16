e19d4fae7f2d30187ea387f78030c737853d46e5
Carter Wright <skycreed04@gmail.com>
2023-11-16 07:16:43 -0500

    Added Meat

--- RobloxCodesPaster.py
+++ RobloxCodesPaster.py
@@ -8,7 +8,77 @@ containing roblox codes (add more on how to format the roblox code text files un
 
 Important Note: Before running this file make sure all dependencies are installed,
 for information on how to install dependencies read the README file.
+
+Potential improvements:
+
 """
+
+
 
 #Imports 
 import pyautogui
+from time import sleep as sleep
+
+#file extraction function
+def data_extractor():
+    """
+    Extracts roblox codes from the RobloxCodesList.txt File and removes white spaces
+    and puts each code into a list and returns that list.
+    
+    Params: None
+
+    Returns: codes_lst(list): List of extracted codes 
+    """
+    #Create empty list to store codes into
+    codes_lst = []
+
+    #Open up the RobloxCodesList.txt file
+    with open('RobloxCodesList.txt', 'r') as file: #DO NOT CHANGE THE FILE NAME
+
+        #Read all lines from file
+        lines = file.readlines()
+        
+        #loops through each line in file
+        for line in lines:
+            #breaks loop if there is empty block spaces
+            if (line.strip() == ""):
+                break
+
+            #cleans up the whitespaces 
+            words = line.strip(' ')
+
+            #sets code_lst to updated words
+            codes_lst = words.split(" ")
+
+            #filters out empty values
+            codes_lst = list(filter(None, codes_lst))
+
+            
+    return codes_lst
+
+
+
+#code pasting function
+def code_paster(lst):
+    """
+    Takes in the extracted codes and pastes each code
+
+    Params: lst(list): The codes_lst that we got from extracting codes from file
+
+    Returns: None
+    """
+    #Give user time to put cursor in the right spot 
+    sleep(5)
+    #Loop through each code in the list
+    for code in lst:
+        pyautogui.write(code)
+        pyautogui.press('enter')
+        pyautogui.hotkey('ctrl', 'a')
+        pyautogui.press('backspace')
+
+#Execution code
+if __name__ == "__main__":
+    #initalizing codes_lst to the list of codes returned by data_extractor()
+    codes_lst = data_extractor()
+    #run the code_paster function to automate pasting codes
+    code_paster(codes_lst)
