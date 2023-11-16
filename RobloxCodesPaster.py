f1080b847d6b75e2c9b301002d5a57510f73477d
shractic <skycreed04@gmail.com>
2023-11-16 05:42:37 -0500

    inital commit

---
+++ RobloxCodesPaster.py
@@ -0,0 +1,14 @@
+"""
+File: Roblox Codes Paster
+Author: Carter Wright
+Date:11/16/2023
+
+Discription: This code file extracts roblox codes from a given text file already
+containing roblox codes (add more on how to format the roblox code text files under)
+
+Important Note: Before running this file make sure all dependencies are installed,
+for information on how to install dependencies read the README file.
+"""
+
+#Imports 
+import pyautogui
